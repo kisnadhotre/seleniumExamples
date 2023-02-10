3f22aff2660ceacb1850504f35ef58e9e3b3c23b
Krisna Dhotre <kisna.dhotre@gmail.com>
2023-02-10 19:57:20 +0530

    add jenkinsfile

--- src/test/java/org/example/SeleniumTest.java
+++ src/test/java/org/example/SeleniumTest.java
@@ -1,51 +1,47 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import junit.framework.Assert;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.CapabilityType;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SeleniumTest {
     WebDriver driver;
 
-    @BeforeTest ()
+    @BeforeMethod (alwaysRun = true)
     public void setUp() {
 //        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
-        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
-
+        //System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver");
+        WebDriverManager.firefoxdriver().setup();
         FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.setBinary("/logs/khk/test1/FIREFOX_BROWSER/firefox/firefox");
+        //firefoxOptions.setBinary("/logs/khk/test1/FIREFOX_BROWSER/firefox/firefox");
         firefoxOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
         firefoxOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-        firefoxOptions.addArguments("--headless", "--enable-javascript", "--enable-touch-drag-drop","--disable-gpu","--no-sandbox");
+        //firefoxOptions.addArguments("--headless", "--enable-javascript", "--enable-touch-drag-drop","--disable-gpu","--no-sandbox");
         driver = new FirefoxDriver(firefoxOptions);
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-        chromeOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-        chromeOptions.addArguments("--headless", "--enable-javascript", "--enable-touch-drag-drop","--disable-gpu","--no-sandbox");
-        driver = new ChromeDriver(chromeOptions);
-
-
     }
 
-    @Test
+    @Test (groups = "sanity")
     public void TestOne() {
         System.out.println("Starting...");
         driver.get("https://www.google.com");
-        System.out.println( "title: " + driver.getTitle() );
+        //System.out.println( "title: " + driver.getTitle() );
+        assertThat(driver.getTitle(), Matchers.equalToIgnoringCase("Google"));
     }
 
-    @Test
+    @Test (groups = "regression")
     public void testTwo() throws Exception {
 
         Robot r = new Robot();
@@ -60,8 +56,8 @@ public class SeleniumTest {
 
     }
 
-    @AfterTest
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        driver.close();
+        driver.quit();
     }
 }
